4e3b37cea7e0af3d95202a3b647ef2e67fac98dd
vsmanivarma <94699976+vsmanivarma@users.noreply.github.com>
2022-11-22 10:32:19 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,8 +13,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-client_id = "402596b9-cf1a-4626-b741-8e270ffcf131"
-tenant_id = "a2ac977c-b09a-474c-8cc0-8159d33c5a34"
-client_secret = "hwe8Q~H~7HW52Yrw7UUqqpx6Dn~VAcN-uQ3myb_."
-subscription_id = "9466e67f-55ff-4afa-ae7f-17d5881721e1"
 }
